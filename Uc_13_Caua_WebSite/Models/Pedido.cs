d2375b09db281ca2e288895c7fa43b08c6cd9dd8
cauagoncalves-p <cauagoncalves2190@gmail.com>
2025-03-25 17:22:28 -0300

    adicionando informações da tabela de produto

--- Uc_13_Caua_WebSite/Models/Pedido.cs
+++ Uc_13_Caua_WebSite/Models/Pedido.cs
@@ -1,6 +1,43 @@
-﻿namespace Uc_13_Caua_WebSite.Models
+﻿using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Uc_13_Caua_WebSite.Models
 {
     public class Pedido
     {
+        [Key]
+        [Display(Name = "ID Pedido")]
+        public int PedidoId { get; set; }
+
+        [Required]
+        [Display(Name = "Cliente")]
+        public int ClienteId { get; set; }
+
+        [ForeignKey("ClienteId")]
+        [Display(Name = "Cliente")]
+        public virtual Cliente? Cliente { get; set; }
+
+        [Required]
+        [Display(Name = "Produto")]
+        public int ProdutoId { get; set; }
+
+        [ForeignKey("ProdutoId")]
+        [Display(Name = "Produto")]
+        public virtual Produto? Produto { get; set; }
+
+        [Required(ErrorMessage = "A data do pedido é obrigatória")]
+        [Display(Name = "Data do Pedido")]
+        [DataType(DataType.DateTime)]
+        public DateTime DataPedido { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "A quantidade é obrigatória")]
+        [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser no mínimo 1")]
+        [Display(Name = "Quantidade")]
+        public int Quantidade { get; set; }
+
+        [Display(Name = "Preço")]
+        [DataType(DataType.Currency)]
+        public decimal Preco { get; set; }
     }
 }
