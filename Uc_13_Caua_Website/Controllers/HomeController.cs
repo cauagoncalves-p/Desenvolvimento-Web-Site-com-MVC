4da73e72fd50e34338e8c94a410a5a74be33a160
cauagoncalves-p <cauagoncalves2190@gmail.com>
2025-03-30 01:30:52 -0300

    colocando as paginas de toucas e truckes

--- Uc_13_Caua_Website/Controllers/HomeController.cs
+++ Uc_13_Caua_Website/Controllers/HomeController.cs
@@ -65,6 +65,14 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult PaginaDeToucas() 
+    {
+        return View();
+    }
+    public IActionResult PaginaDeTruckes() 
+    {
+        return View();  
+    }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
